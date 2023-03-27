ddb0a3a2ecd107749e1764a436db5a16776b197f
junyoung kim <adad568@naver.com>
2023-03-27 23:23:42 +0900

    Init project

---
+++ matched/src/main/java/com/linked/matched/MatchedApplication.java
@@ -0,0 +1,13 @@
+package com.linked.matched;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchedApplication.class, args);
+	}
+
+}
